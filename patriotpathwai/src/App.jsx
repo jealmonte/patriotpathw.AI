28b1ec5b33b9c034df45a0491bb81b9e6e753ac4
lawrencelin1154 <lawrencelin1154@gmail.com>
2024-10-12 03:09:30 -0400

    added signup to questionaire

--- patriotpathwai/src/App.jsx
+++ patriotpathwai/src/App.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
-import { AuthProvider } from '@propelauth/react';
+import { AuthProvider, useAuthInfo, useRedirectFunctions } from '@propelauth/react';
+import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
 import ChatPage from './Pages/ChatPage';
-import { Box } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Questionnaire from './Components/Questionaire.jsx';
 
 function App() {
   return (
     <Box width="100vw" height="100vh" overflow='auto'>
-    <AuthProvider authUrl="https://59746679014.propelauthtest.com">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/chatpage" element={<ChatPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+      <AuthProvider authUrl="https://59746679014.propelauthtest.com">
+        <AppRoutes />
+      </AuthProvider>
     </Box>
+  );
+}
+
+function AppRoutes() {
+  const { redirectToSignupPage, redirectToLoginPage } = useRedirectFunctions();
+
+  const handleSignup = () => {
+    redirectToSignupPage({
+      postSignupRedirectUrl: 'http://localhost:5173/SignUp/Questionnaire'
+    });
+  };
+
+  const handleLogin = () => {
+    redirectToLoginPage({
+      postLoginRedirectUrl: '/chatpage'
+    });
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home onSignup={handleSignup} onLogin={handleLogin} />} />
+        <Route path="/chatpage" element={<ChatPage />} />
+        <Route path="/SignUp/Questionnaire" element={<Questionnaire />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
